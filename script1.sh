5e8d31b39bfa25a0afc39c369a15a90657ae3036
ganesh <ganeshkonda763@gmail.com>
2023-05-15 20:34:09 +0530

    update

---
+++ script1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "TESTING SCRIPT"
+echo
+df -h
+echo
+uname -a
+echo
+date
+echo
+ifconfig -a
+echo
+who -r
+
---
+++ script3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Enter A value = "
+read a
+echo "Enter B value = "
+read b
+echo
+c=`expr $a + $b`
+echo "c value =$c"
